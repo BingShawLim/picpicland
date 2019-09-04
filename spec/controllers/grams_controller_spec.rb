4d9c23c538cf9e5d9e9d924d1b3da51a5f702d31
Bing Shaw Lim <shaw.a.muvee@gmail.com>
2019-09-04 13:52:59 +0000

    Lesson 11: the evil {}

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe GramsController, type: :controller do
     end
   end
 
+
   describe "grams#new action" do
     it "should require users to be logged in" do
       get :new
@@ -15,11 +16,7 @@ RSpec.describe GramsController, type: :controller do
     end
 
     it "should successfully show the new form" do
-       user = User.create(
-        email:                 'fakeuser@gmail.com',
-        password:              'secretPassword',
-        password_confirmation: 'secretPassword'
-      )
+      user = FactoryBot.create(:user)
       sign_in user
 
       get :new
@@ -27,40 +24,34 @@ RSpec.describe GramsController, type: :controller do
     end
   end
 
-describe "grams#create action" do
-  it "should require users to be logged in" do
-    post :create, params: { gram: { message: "hello"} }
-    expect(response).to redirect_to new_user_session_path
-  end
 
+  describe "grams#create action" do
 
-  it "should successfully create a new gram in our database" do
-     user = User.create(
-        email:                 'fakeuser@gmail.com',
-        password:              'secretPassword',
-        password_confirmation: 'secretPassword'
-      )
+    it "should require users to be logged in" do
+      post :create, params: { gram: { message: "Hello" } }
+      expect(response).to redirect_to new_user_session_path
+    end
+
+    it "should successfully create a new gram in our database" do
+      user = FactoryBot.create(:user)
       sign_in user
 
-    post :create, params: { gram: { message: 'Hello!' } }
-    expect(response).to redirect_to root_path
+      post :create, params: { gram: { message: 'Hello!' } }
+      expect(response).to redirect_to root_path
 
-    gram = Gram.last
-    expect(gram.message).to eq("Hello!")
-    expect(gram.user).to eq(user)
+      gram = Gram.last
+      expect(gram.message).to eq("Hello!")
+      expect(gram.user).to eq(user)
     end
 
     it "should properly deal with validation errors" do
-       user = User.create(
-        email:                 'fakeuser@gmail.com',
-        password:              'secretPassword',
-        password_confirmation: 'secretPassword'
-      )
+      user = FactoryBot.create(:user)
       sign_in user
 
-      post :create, params: { gram: {message: '' } }
+      gram_count = Gram.count
+      post :create, params: { gram: { message: '' } }
       expect(response).to have_http_status(:unprocessable_entity)
-      expect(Gram.count).to eq Gram.count
+      expect(gram_count).to eq Gram.count
     end
 
 
